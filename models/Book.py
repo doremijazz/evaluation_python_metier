5e4ef5a8a215f7edd36200473765514618f87b60
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-14 14:10:21 +0200

    Implémentation de la classe et des trois méthodes d'affichage str, all, resume

--- models/Book.py
+++ models/Book.py
@@ -1,4 +1,25 @@
+from models import Author
+from datetime import date
+
+
 class Book:
     title: str
-    author: str
-    
+    author: Author
+    resume: str
+    publish_date: date
+    editeur : str
+    pp : list[str]
+    nb_pages : int
+    isbn : int
+    price : float
+
+    def _str_(self):
+        return  (f"{self.title} by {self.author} editing by {self.editeur} published on {self.publish_date},"
+                 f" nombre de pages {self.nb_pages}, isbn {self.isbn}, price {self.price}")
+
+    def display_all_books(list:list[Book]):
+        for book in list:
+            print(f"{book.title} by {book.author}")
+            
+    def display_resume(self):
+        print(f"resume {self.resume}")

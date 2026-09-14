28cb439f6c09726728b7bd90d56cea505dd15262
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-14 15:16:36 +0200

    Méthode display dans member.py

--- models/Member.py
+++ models/Member.py
@@ -9,6 +9,10 @@ class Member(User):
     member_id: ClassVar[int] = 0
     member_nbr: int = field(init=False)
 
+    def display(self):
+        person_str = super().__str__()
+        print(f"Member_ID : {self.member_id}, Information : {person_str}")
+
 
     def vote(self,books:list[Book], nbr: int)-> list[Book]:
         selected_books = []

45281d1c9439e879f17915bf1e0ce55f6f90be1e
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-14 14:15:16 +0200

    Ajout de l'instenciation

--- models/President.py
+++ models/President.py
@@ -4,8 +4,10 @@ from typing import ClassVar
 from models.Book import Book
 from models.User import User
 
-
+@dataclass
 class president(User):
+    def __init__(self,username:str,password:str,email:str):
+        super().__init__(username,password,email)
 
     def display(self):
         pass

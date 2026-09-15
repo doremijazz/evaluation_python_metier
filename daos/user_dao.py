19035ca4df54efaaaea5618536d642c825729937
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-15 13:01:21 +0200

    Ajout des methodes a user_dao en attente d'implémentation

---
+++ daos/user_dao.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from daos.dao import Dao
+from models.User import User
+
+
+@dataclass
+class MemberDao(Dao[User]):
+    def create(self, user: User) -> int:
+        pass
+    def read(self, user_id: int) -> User:
+        pass
+    def readAll(self) -> list[User]:
+        pass
+    def update(self, user: User) -> bool:
+        pass
+    def delete(self, user: User) -> bool:
+        pass

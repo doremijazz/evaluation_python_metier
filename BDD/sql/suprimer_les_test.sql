ec150d0f1fbca73dd9591848d66892714f6814a1
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-16 14:59:11 +0200

    Ajout d'un fichier sql pour suprimer tous les artefacte de test en dehors des données obligatoir

---
+++ BDD/sql/suprimer_les_test.sql
@@ -0,0 +1,5 @@
+DELETE FROM pg_membre WHERE m_ID_membre >9;
+DELETE FROM pg_user WHERE u_ID_user > 10;
+DELETE FROM pg_auteur WHERE aut_ID_auteur >16;
+DELETE FROM pg_person WHERE p_ID_person >26;
+DELETE FROM pg_livre WHERE l_ID_livre >16;

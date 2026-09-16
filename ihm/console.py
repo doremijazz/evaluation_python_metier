c64cfda206ae12b49dde7e5323a9522100ee7d6b
Anaïs BINET <anais.binet@fms-ea.com>
2026-09-16 16:30:50 +0200

    IMPL : menu console pour admin, president et membre

---
+++ ihm/console.py
@@ -0,0 +1,33 @@
+class console:
+    @staticmethod
+    def president_menu():
+        print("##########################\n")
+        print("----- MENU PRESIDENT ------")
+        print("#########################\n")
+        print("1- Afficher tous les livres\n")
+        print("2- Afficher tous les membres\n")
+        print("3- Saisir les votes\n")
+        print("4- Saisir la selection de livre pour le prochian tour\n")
+        print("0- Quitter\n")
+
+    def admin_menu():
+        print("#########################\n")
+        print("----- MENU ADMIN ------")
+        print("#########################\n")
+        print("1- Ajouter un membre\n")
+        print("2- Modifier un membres\n")
+        print("3- Suprimer un membre\n")
+        print("4- Ajouter un livre\n")
+        print("5- Modifier un livre\n")
+        print("6- Suprimer un livre\n")
+        print("0- Quitter\n")
+
+    def member_menu():
+        print("########################\n")
+        print("----- MENU MEMBER ------")
+        print("########################\n")
+        print("1- Afficher tous les livres\n")
+        print("2- Voter pour une selection de livre\n")
+        print("0- Quitter\n")
+
+
